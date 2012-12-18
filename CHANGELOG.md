629c8e4c5251f6125be9f6eacecb1369fcb1cd4c
jtimberman <joshua@opscode.com>
2012-12-18 14:52:45 -0700

    release v1.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v1.0.2:
+
+* [COOK-1965] - fixes template subscribes and readme typos
+
 ## v1.0.0:
 
 * [COOK-1656] - Make keepalived configurable. Add some tests.
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs and configures keepalived"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "1.0.0"
+version           "1.0.2"
 supports          "ubuntu"
 
 recipe "keepalived", "Installs and configures keepalived"
