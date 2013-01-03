687336a4c9374fd05e04522e554446649e6a4085
galstrom21 <jshepher@rackspace.com>
2013-01-03 17:53:02 -0600

    cleaned up the dirty bit

--- providers/vrrp.rb
+++ providers/vrrp.rb
@@ -1,17 +1,13 @@
 
 action :create do
-  # This is dirty.. needs to be redone
   params = Hash.new()
-  params['interface'] = new_resource.interface
-  params['virtual_router_id'] = new_resource.virtual_router_id
-  params['state'] = new_resource.state
-  params['noprempt'] = new_resource.noprempt
-  params['priority'] = new_resource.priority
-  params['virtual_ipaddress'] = new_resource.virtual_ipaddress
-  params['advert_int'] = new_resource.advert_int if new_resource.advert_int
-  params['auth_type'] = new_resource.auth_type if new_resource.auth_type
-  params['auth_pass'] = new_resource.auth_pass
-  params['track_script'] = new_resource.track_script if new_resource.track_script
+  attributes = ['interface', 'virtual_router_id', 'state', 'nopreempt', 'priority', 'virtual_ipaddress', 'advert_int', 'auth_type', 'auth_pass', 'track_script']
+  attributes.each do |attribute|
+    if new_resource.respond_to?(attribute)
+      params[attribute] = new_resource.send(attribute.to_sym)
+    end
+  end
+
 
   template "vrrp_#{new_resource.name.upcase}" do
     path "/etc/keepalived/conf.d/vrrp_#{new_resource.name.upcase}.conf"
