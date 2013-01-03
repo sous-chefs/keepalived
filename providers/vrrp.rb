786b6ad2f8f46a09e4699c2bd68a273d0b6ffd73
galstrom21 <jshepher@rackspace.com>
2013-01-03 14:46:28 -0600

    keepalived_vrrp LWRP

---
+++ providers/vrrp.rb
@@ -0,0 +1,30 @@
+
+action :create do
+  # This is dirty.. needs to be redone
+  params = Hash.new()
+  params['interface'] = new_resource.interface
+  params['virtual_router_id'] = new_resource.virtual_router_id
+  params['state'] = new_resource.state
+  params['noprempt'] = new_resource.noprempt
+  params['priority'] = new_resource.priority
+  params['virtual_ipaddress'] = new_resource.virtual_ipaddress
+  params['advert_int'] = new_resource.advert_int if new_resource.advert_int
+  params['auth_type'] = new_resource.auth_type if new_resource.auth_type
+  params['auth_pass'] = new_resource.auth_pass if new_resource.auth_pass
+  params['track_script'] = new_resource.track_script if new_resource.track_script
+
+  template new_resource.name do
+    path "/etc/keepalived/conf.d/vrrp_#{new_resource.name}.conf"
+    source "vrrp_generic.conf.erb"
+    cookbook "keepalived"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(
+      "name" => new_resource.name,
+      "params" => params
+    )
+    notifies :restart, resource(:service => "keepalived"), :delayed
+  end
+  new_resource.updated_by_last_action(true)
+end
---
+++ resources/vrrp.rb
@@ -0,0 +1,14 @@
+
+
+attribute :interface, :kind_of => String, :required => true
+attribute :virtual_router_id, :kind_of => Integer, :default => 10
+attribute :state, :kind_of => String, :equal_to => ["MASTER", "BACKUP"], :default => "BACKUP"
+attribute :noprempt, :kind_of => [TrueClass, FalseClass], :default => false
+attribute :priority, :kind_of => Integer, :default => 100
+attribute :virtual_ipaddress, :kind_of => Array, :required => true
+
+# Optional attributes
+attribute :advert_int, :kind_of => Integer, :required => false
+attribute :auth_type, :kind_of => String, :equal_to => ["pass", "ah"], :required => false
+attribute :auth_pass, :kind_of => String, :required => false
+attribute :track_script, :kind_of => String, :required => false
