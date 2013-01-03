3d1d25e53679102ca290b4243f8369671dfef988
galstrom21 <jshepher@rackspace.com>
2013-01-03 17:23:44 -0600

    Creating simple keepalived_virtual_server LWRP

---
+++ providers/virtual_server.rb
@@ -0,0 +1,22 @@
+
+action :create do
+  template "vs_#{new_resource.name}" do
+    path "/etc/keepalived/conf.d/vs_#{new_resource.name}.conf"
+    source "vs_generic.conf.erb"
+    cookbook "keepalived"
+    owner "root"
+    group "root"
+    mode "0644"
+    variables(
+      "vs_listen_ip" => new_resource.vs_listen_ip,
+      "vs_listen_port" => new_resource.vs_listen_port,
+      "delay_loop" => new_resource.delay_loop,
+      "lb_algo" => new_resource.lb_algo,
+      "lb_kind" => new_resource.lb_kind,
+      "vs_protocol" => new_resource.vs_protocol,
+      "real_servers" => new_resource.real_servers
+    )
+    notifies :restart, resources(:service => "keepalived"), :delayed
+  end
+  new_resource.updated_by_last_action(true)
+end
---
+++ resources/virtual_server.rb
@@ -0,0 +1,18 @@
+
+actions :create
+
+default_action :create
+
+# Covers 0.10.8 and earlier
+def initialize(*args)
+  super
+  @action = :create
+end
+
+attribute :vs_listen_ip, :kind_of => String, :required => true
+attribute :vs_listen_port, :kind_of => String, :required => true
+attribute :delay_loop, :kind_of => Integer, :default => 15
+attribute :lb_algo, :kind_of => String, :equal_to => ["rr", "wrr", "lc", "wlc", "sh", "dh", "lblc"], :default => "rr"
+attribute :lb_kind, :kind_of => String, :equal_to => ["nat", "dr", "tun"], :default => "nat"
+attribute :vs_protocol, :kind_of => String, :equal_to => ["tcp", "udp"], :default => "tcp"
+attribute :real_servers, :kind_of => Array, :required => true
