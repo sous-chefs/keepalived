2458195a6d91aca4ce495249de30f787751fd2a8
rackerjoe <breu@breu.org>
2013-05-30 18:34:53 -0500

    foodcritic fixes

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,3 +15,9 @@ default["keepalived"]["vs_defaults"]["lb_algo"] = "rr"
 default["keepalived"]["vs_defaults"]["lb_kind"] = "nat"  # Valid options are nat, dr, and tun
 default["keepalived"]["vs_defaults"]["delay_loop"] = 15
 default["keepalived"]["vs_defaults"]["protocol"] = "tcp"  # Valid options are tcp or udp
+
+if platform_family?("rhel")
+  default["keepalived"]["service_bin"] = "/sbin/service"
+else
+  default["keepalived"]["service_bin"] = "/usr/sbin/service"
+end
