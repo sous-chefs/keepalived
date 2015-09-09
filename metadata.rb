5a2443c2ad29bc04bdcd0ca8596b51a8d87b5da9
Tim Smith <tsmith84@gmail.com>
2015-09-09 10:22:20 -0700

    Add debian support and supermarket metadata

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,9 @@ description       "Installs and configures keepalived"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "1.2.0"
 supports          "ubuntu"
+supports          "debian"
 
 recipe "keepalived", "Installs and configures keepalived"
+
+source_url 'https://github.com/chef-cookbooks/keepalived' if respond_to?(:source_url)
+issues_url 'https://github.com/chef-cookbooks/keepalived/issues' if respond_to?(:issues_url)
