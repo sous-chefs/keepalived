4cc0a3c7bb2b6a09f41304d8ccce59801cc9d72f
Tim Smith <tsmith84@gmail.com>
2015-10-21 11:32:24 -0700

    Add RHEL distros

--- metadata.rb
+++ metadata.rb
@@ -5,10 +5,13 @@ license           'Apache 2.0'
 description       'Installs and configures keepalived'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           '1.2.0'
-supports          'ubuntu'
-supports          'debian'
 
 recipe 'keepalived', 'Installs and configures keepalived'
+
+%w(ubuntu debian redhat centos scientific oracle amazon).each do |os|
+  supports os
+end
+
 
 source_url 'https://github.com/chef-cookbooks/keepalived' if respond_to?(:source_url)
 issues_url 'https://github.com/chef-cookbooks/keepalived/issues' if respond_to?(:issues_url)
