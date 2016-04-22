7194eb5213db6306261d9ba324d5819004e7cf9d
Nathan Williams <nathan@teamtreehouse.com>
2016-04-22 12:23:00 -0700

    add specs for keepalived-options file

--- spec/unit/recipes/configure_spec.rb
+++ spec/unit/recipes/configure_spec.rb
@@ -3,9 +3,7 @@ require 'spec_helper'
 describe 'keepalived::configure' do
   context 'resource-driven' do
     let(:chef_run) do
-      ChefSpec::ServerRunner.new do |node|
-        node.set['keepalived']['use_attributes'] = false
-      end.converge(described_recipe)
+      ChefSpec::ServerRunner.new.converge(described_recipe)
     end
 
     it 'creates the resource-driven resource include dirs' do
@@ -17,12 +15,36 @@ describe 'keepalived::configure' do
         )
       end
 
+      expect(chef_run).to_not create_file('keepalived-options')
+
       expect(chef_run).to create_file('keepalived.conf').with(
         path: '/etc/keepalived/keepalived.conf',
         content: "include /etc/keepalived/conf.d/*.conf\n",
         owner: 'root',
         group: 'root',
         mode: '0640'
+      )
+    end
+
+    it 'converges successfully' do
+      chef_run # This should not raise an error
+    end
+  end
+
+  context 'daemon-args' do
+    let(:chef_run) do
+      ChefSpec::ServerRunner.new do |node|
+        node.set['keepalived']['daemon_args'] = %w( -D --snmp )
+      end.converge(described_recipe)
+    end
+
+    it 'creates the defaults file' do
+      expect(chef_run).to create_file('keepalived-options').with(
+        path: '/etc/sysconfig/keepalived',
+        owner: 'root',
+        group: 'root',
+        mode: '0640',
+        content: "KEEPALIVED_OPTIONS='-D --snmp'"
       )
     end
 
