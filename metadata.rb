e078e176a06fc3b57a6c8ace507a53e40be5672b
Dan Webb <dan.webb@damacus.io>
2019-08-16 10:20:19 +0100

    Update kitchen and circleCI config

--- metadata.rb
+++ metadata.rb
@@ -4,17 +4,14 @@ maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs and configures keepalived'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url        'https://github.com/sous-chefs/keepalived'
+issues_url        'https://github.com/sous-chefs/keepalived/issues'
 version           '3.1.1'
 
-recipe 'keepalived::default', 'Install, configure, and run keepalived'
-recipe 'keepalived::install', 'Install keepalived package'
-recipe 'keepalived::configure', 'Configure keepalived via attributes'
-recipe 'keepalived::service', 'Enable, start the keepalived service'
-
-%w(ubuntu debian redhat centos scientific oracle amazon).each do |os|
-  supports os
-end
-
-source_url 'https://github.com/chef-cookbooks/keepalived'
-issues_url 'https://github.com/chef-cookbooks/keepalived/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+supports 'ubuntu'
+supports 'debian'
+supports 'redhat'
+supports 'centos'
+supports 'scientific'
+supports 'oracle'
+supports 'amazon'
