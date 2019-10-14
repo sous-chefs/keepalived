7b3e76123d20ebfcc501e8865142a31a3565cfb3
Dan Webb <dan.webb@damacus.io>
2019-10-14 20:25:00 +0100

    Cookstyle fixes!
    Thanks @tas50

--- metadata.rb
+++ metadata.rb
@@ -3,13 +3,8 @@ maintainer        'Chef Software, Inc.'
 maintainer_email  'cookbooks@chef.io'
 license           'Apache-2.0'
 description       'Installs and configures keepalived'
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           '3.1.1'
 
-recipe 'keepalived::default', 'Install, configure, and run keepalived'
-recipe 'keepalived::install', 'Install keepalived package'
-recipe 'keepalived::configure', 'Configure keepalived via attributes'
-recipe 'keepalived::service', 'Enable, start the keepalived service'
+version           '3.1.1'
 
 %w(ubuntu debian redhat centos scientific oracle amazon).each do |os|
   supports os
@@ -17,4 +12,4 @@ end
 
 source_url 'https://github.com/chef-cookbooks/keepalived'
 issues_url 'https://github.com/chef-cookbooks/keepalived/issues'
-chef_version '>= 12.1' if respond_to?(:chef_version)
+chef_version '>= 12.1'
