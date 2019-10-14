ff675141250acb65c2ebb0df9b2b0417b291cc3b
Dan Webb <dan.webb@damacus.io>
2019-10-14 20:31:37 +0100

    Tidy up metadata

--- metadata.rb
+++ metadata.rb
@@ -1,15 +1,13 @@
-name              'keepalived'
-maintainer        'Chef Software, Inc.'
-maintainer_email  'cookbooks@chef.io'
-license           'Apache-2.0'
-description       'Installs and configures keepalived'
-
-version           '3.1.1'
+name             'keepalived'
+maintainer       'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license          'Apache-2.0'
+description      'Installs and configures keepalived'
+source_url       'https://github.com/chef-cookbooks/keepalived'
+issues_url       'https://github.com/chef-cookbooks/keepalived/issues'
+chef_version     '>= 12.1'
+version          '3.1.1'
 
 %w(ubuntu debian redhat centos scientific oracle amazon).each do |os|
   supports os
 end
-
-source_url 'https://github.com/chef-cookbooks/keepalived'
-issues_url 'https://github.com/chef-cookbooks/keepalived/issues'
-chef_version '>= 12.1'
